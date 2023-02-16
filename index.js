6b1012fc29fc0800db36d703dbc3afcc0ddfd675
Bob <bob@projectbabbage.com>
2023-02-16 22:25:19 +0000

    initial update WIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const parapet = require('parapet-js')
+// const parapet = require('parapet-js')
 const { getHashFromURL } = require('uhrp-url')
 const crypto = require('crypto')
 const fetch = require('isomorphic-fetch')
@@ -8,25 +8,37 @@ const fetch = require('isomorphic-fetch')
  *
  * @param {Object} obj All parameters are passed in an object.
  * @param {String} obj.URL The UHRP URL to resolve.
- * @param {Array<String>} [obj.trustedHosts=['15RLMCYZ738Y3cBb56yDSWa7TkAFxSQtyf']] A set of UHRP hosts given preferential treatmeant. By default, the Babbage host is trusted. Provide an empty array to resolve all URLs.
- * @param {Number} [obj.limit=10] The number of results to return.
- * @param {Number} [obj.offset=0] The offset into the total number of results.
- * @param {Array} [obj.bridgeportResolvers] Specify custom Bridgeport resolvers
  *
  * @return {Array<String>} An array of HTTP URLs where content can be downloaded.
  */
 const resolve = async ({
-  URL,
+  URL
+  /* ,
   trustedHosts = ['15RLMCYZ738Y3cBb56yDSWa7TkAFxSQtyf'],
   limit = 10,
   offset = 0,
-  bridgeportResolvers
+  bridgeportResolvers */
 } = {}) => {
+
+  // The URL must not contain a " "
+  if (URL.indexOf(' ') !== -1) {
+    const e = new Error('URL contains a space')
+    e.code = 'ERR_INVALID_HTTP_SPACE_URL'
+    throw e
+  }
+  // The URL must contain a "."
+  if (URL.indexOf('.') === -1) {
+    const e = new Error('URL does not contain a dot')
+    e.code = 'ERR_INVALID_HTTP_DOT_URL'
+    throw e
+  }
+
+
   // The hash is extracted from the UHRP URL
   const hash = getHashFromURL(URL).toString('hex')
 
   // The query is for URLs of matching hashes that are not revoked or expired
-  const query = {
+  /* const query = {
     v: 3,
     q: {
       collection: 'content',
@@ -42,22 +54,23 @@ const resolve = async ({
       limit,
       skip: offset
     }
-  }
+  } */
 
   // If there are trusted hosts, the query only includes them
-  if (trustedHosts.length > 0) {
+  /* if (trustedHosts.length > 0) {
     query.q.find.host = { $in: trustedHosts }
-  }
+  } */
+
 
   // The query is run against the UHRP Bridge
-  const resolved = await parapet({
+  /* const resolved = await parapet({
     bridge: '1AJsUZ7MsJGwmkCZSoDpro28R52ptvGma7',
     resolvers: bridgeportResolvers,
     request: {
       type: 'json-query',
       query
     }
-  })
+  }) */
 
   // If there are no results within a set of trusted hosts, re-run without trusted host restrictions
   if (resolved.length === 0 && trustedHosts.length > 0) {
@@ -80,23 +93,31 @@ const resolve = async ({
  *
  * @param {Object} obj All parameters are passed in an object.
  * @param {String} obj.URL The UHRP URL to download.
- * @param {Array<String>} [obj.trustedHosts=['15RLMCYZ738Y3cBb56yDSWa7TkAFxSQtyf']] A set of UHRP hosts given preferential treatmeant. By default, the Babbage host is trusted. Provide an empty array to disable trusted host resolution.
- * @param {Number} [obj.limit=10] The maximum number of URLs to try downloading from before giving up.
- * @param {Number} [obj.offset=0] The offset into the list of potential download URLs to start from.
- * @param {Array} [obj.bridgeportResolvers] Specify custom Bridgeport resolvers
  *
  * @return {Object} An object containing "data" (a buffer) and "mimeType" for the content.
  */
 const download = async ({
-  URL,
-  trustedHosts = ['15RLMCYZ738Y3cBb56yDSWa7TkAFxSQtyf'],
-  bridgeportResolvers
+  URL
 } = {}) => {
+  // The URL must not contain a " "
+  if (URL.indexOf(' ') !== -1) {
+    const e = new Error('URL contains a space')
+    e.code = 'ERR_INVALID_HTTP_SPACE_URL'
+    throw e
+  }
+  // The URL must contain a "."
+  if (URL.indexOf('.') === -1) {
+    const e = new Error('URL does not contain a dot')
+    e.code = 'ERR_INVALID_HTTP_DOT_URL'
+    throw e
+  }
+
   // The hash is extracted from the UHRP URL for later validation
   const hash = getHashFromURL(URL).toString('hex')
 
   // A list of potential download URLs are resolved
-  const URLs = await resolve({ URL, trustedHosts, bridgeportResolvers })
+  const URLs = await resolve({ URL })
+  //const URLs = await resolve({ URL, trustedHosts, bridgeportResolvers })
 
   // Download is attempted from each URL until successful
   for (let i = 0; i < URLs.length; i++) {
@@ -137,7 +158,9 @@ const download = async ({
   }
 
   // If the loop finishes without success, the content cannot be downloaded
-  throw new Error(`Unable to download content from ${URL}`)
+  const e =  new Error(`Unable to download content from ${URL}`)
+  e.code = 'ERR_UNABLE_TO_DOWNLOAD'
+  throw e
 }
 
-module.exports = { resolve, download }
+module.exports = { download }
